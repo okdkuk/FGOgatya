42a81492e76130719e04b2ea071cb64b5160e111
okdkuk <e175701@ie.u-ryukyu.ac.jp>
2018-01-16 03:42:20 +0900

    2nd

---
+++ src/main/java/jp/ac/uryukyu/ie/e175701/Main.java
@@ -0,0 +1,10 @@
+import jp.ac.uryukyu.ie.e175701.*;
+
+public class Main {
+    public static void main(String[] arms ){
+
+        DoGatya doGatya = new DoGatya();
+        for(int i = 0; i < 10 ; i++)
+        doGatya.DoGtya();
+    }
+}
